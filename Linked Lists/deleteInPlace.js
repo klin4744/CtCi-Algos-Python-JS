54fc3a9ff7bd3dcbe521b25cbfc674e0f9e57067
Kevin Lin <kevin.lin.2@stonybrook.edu>
2019-11-23 21:56:34 -0500

    delete in place

---
+++ Linked Lists/deleteInPlace.js
@@ -0,0 +1,27 @@
+// Implement an algorithm to find the kth last element of a singly linked list
+
+class LinkedListNode {
+   constructor(val) {
+      this.val = val;
+      this.next = null;
+   }
+}
+
+function deleteInPlace(nodeToDelete) {
+   nodeToDelete.val = nodeToDelete.next.val;
+   nodeToDelete.next = nodeToDelete.next.next;
+}
+//        *     *
+// 1, 2, 3 , 4 ,5   2
+const L1 = new LinkedListNode('a');
+const L2 = new LinkedListNode('b');
+const L3 = new LinkedListNode('c');
+const L4 = new LinkedListNode('b');
+const L5 = new LinkedListNode('c');
+L1.next = L2;
+L2.next = L3;
+L3.next = L4;
+L4.next = L5;
+
+console.log(deleteInPlace(L3));
+console.log(L1);
